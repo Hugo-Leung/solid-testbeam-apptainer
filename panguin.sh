f1dfe7ec06b8bb4c39c988c0bbca8411db905438
Ching Him Leung <cleung@jlab.org>
2026-03-17 21:20:00 -0400

    new file:   panguin.sh

---
+++ panguin.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e 
+cd /opt
+
+if  [[ -z "$ROOTSYS" ]]; then
+	source /opt/root/bin/thisroot.sh
+fi
+export ANALYZER="/opt/analyzer"
+export PATH=/opt/analyzer/bin:$PATH
+export LD_LIBRARY_PATH=/opt/analyzer/lib64:$LD_LIBRARY_PATH
+
+VERSION="main"
+
+git clone --depth=1  --branch ${VERSION} git@github.com:JeffersonLab/panguin.git  
+
+cd panguin
+mkdir build 
+cd build
+cmake ../
+
+njobs=5
+ncpu=$(nproc)
+if [[ ${ncpu} -lt ${njobs} ]]; then
+	njobs=${cpu}
+fi
+cmake --build build -j${njobs}
